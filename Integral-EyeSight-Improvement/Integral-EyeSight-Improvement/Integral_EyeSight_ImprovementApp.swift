7e397b6defe72d5b002dffeb9ceb4254d955595b
Roman Kozulia <romanmuni8@gmail.com>
2023-12-10 15:44:46 -0500

    Project setup

---
+++ Integral-EyeSight-Improvement/Integral-EyeSight-Improvement/Integral_EyeSight_ImprovementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Integral_Eyesight_ImprovementApp.swift
+//  Integral-Eyesight-Improvement
+//
+//  Created by Roman Kozulia on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Integral_Eyesight_ImprovementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Integral-EyeSight-Improvement/Integral-EyeSight-ImprovementUITests/Integral_EyeSight_ImprovementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Integral_Eyesight_ImprovementUITestsLaunchTests.swift
+//  Integral-Eyesight-ImprovementUITests
+//
+//  Created by Roman Kozulia on 12/10/23.
+//
+
+import XCTest
+
+final class Integral_Eyesight_ImprovementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
